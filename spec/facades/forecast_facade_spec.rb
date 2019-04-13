cdee7976d545960533ac4f51a0fd69325fc00c44
Julia Marco <marcootj@gmail.com>
2019-04-13 15:53:59 -0600

    Added weather data and forecast_data test and functionality for facade

--- spec/facades/forecast_facade_spec.rb
+++ spec/facades/forecast_facade_spec.rb
@@ -22,5 +22,16 @@ describe ForecastFacade do
       expect(location.latitude).to eq(39.7392358)
       expect(location.longitude).to eq(-104.990251)
     end
+
+    it '#get_forecast_data' do
+      forecast_data = @facade.get_forecast_data(39.7392358, -104.990251)
+      expect(forecast_data).to have_key(:currently)
+    end
+
+    it '#weather_data' do
+      weather_data = @facade.weather_data
+      expect(weather_data).to be_a(Weather)
+      expect(weather_data.date_time).to eq(DateTime.now.strftime("%I:%M %p, %d/%m"))
+    end
   end
 end
