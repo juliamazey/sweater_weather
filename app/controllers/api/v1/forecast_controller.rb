ffb51aa2277a61f9c24f21c0991437c2e8fdfab5
Julia Marco <marcootj@gmail.com>
2019-04-13 15:45:54 -0600

    BDD to pass weather object into serializer

--- app/controllers/api/v1/forecast_controller.rb
+++ app/controllers/api/v1/forecast_controller.rb
@@ -2,7 +2,9 @@ class Api::V1::ForecastController < ApplicationController
 
   def show
     facade = ForecastFacade.new(params[:location])
+    weather = facade.weather_data
 
+    render json: ForecastSerializer.new(weather)
   end
 
 end
