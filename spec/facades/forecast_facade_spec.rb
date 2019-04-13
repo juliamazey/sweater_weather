9635d1455141e50b2294ddea15f6d00d92c487f7
Julia Marco <marcootj@gmail.com>
2019-04-13 15:29:38 -0600

    Updated facade spec

--- spec/facades/forecast_facade_spec.rb
+++ spec/facades/forecast_facade_spec.rb
@@ -12,6 +12,15 @@ describe ForecastFacade do
   describe "instance methods" do
     it '#geo_data' do
       data = @facade.geo_data
+      expect(data[:lat]).to eq(39.7392358)
+      expect(data[:lng]).to eq(-104.990251)
+    end
+
+    it '#get_location' do
+      location = @facade.get_location
+      expect(location.address).to eq(@address)
+      expect(location.latitude).to eq(39.7392358)
+      expect(location.longitude).to eq(-104.990251)
     end
   end
 end
