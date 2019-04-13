ca5e1369681cadbf6d7da7cbd49ae660f131373e
Julia Marco <marcootj@gmail.com>
2019-04-13 15:35:05 -0600

    Added get_forecast_method to facade

--- app/facades/forecast_facade.rb
+++ app/facades/forecast_facade.rb
@@ -8,11 +8,16 @@ class ForecastFacade
     geo_service.get_lat_long(@address)[:results][0][:geometry][:location]
   end
 
-  def get_location
+  def get_location()
     location = Location.find_by(address: @address)
     if location.nil?
       location = Location.create_location(@address, geo_data)
     end
+  end
+
+  def get_forecast_data(latitude, longitude)
+    dark_sky_service = DarkSkyService.new(latitude, longitude)
+    dark_sky_service.get_forecast
   end
 
 end
