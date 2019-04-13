ce89a58ea093ec4683582ff33fc193de7b04b60b
Julia Marco <marcootj@gmail.com>
2019-04-13 15:22:45 -0600

    Added get location method to facade

--- app/facades/forecast_facade.rb
+++ app/facades/forecast_facade.rb
@@ -4,8 +4,15 @@ class ForecastFacade
   end
 
   def geo_data
-    geo_service = GeocodeService.new(@address)
-    geo_service.get_lat_long[:results][0][:geometry][:location]
+    geo_service = GeocodeService.new
+    geo_service.get_lat_long(@address)[:results][0][:geometry][:location]
+  end
+
+  def get_location
+    location = Location.find_by(address: @address)
+    if location.nil?
+      location = Location.create_location(@address, geo_data)
+    end
   end
 
 end
