c56c62eb2bb56ea76475726908f156f2f7342b76
Julia Marco <marcootj@gmail.com>
2019-04-14 09:58:12 -0600

    Added user model and validations

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,6 +1,13 @@
 class Api::V1::UsersController < ApplicationController
 
    def create
+     user = User.find_or_create_by(user_params)
+   end
+
+   private
+
+   def user_params
+     params.permit(:email, :password, :password_confirmation)
    end
 
 end
---
+++ app/models/user.rb
@@ -0,0 +1,7 @@
+class User < ApplicationRecord
+  has_secure_password
+
+  validates :email, uniqueness: true, presence: true
+  validates :api_key, uniqueness: true, presence: true
+  validates_presence_of :password
+end
