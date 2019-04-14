2fd2340dc3ee113db414e38b1aeabd33d8cc703a
Julia Marco <marcootj@gmail.com>
2019-04-14 11:05:25 -0600

    Finished log in functionality

--- spec/requests/api/v1/user_login_request_spec.rb
+++ spec/requests/api/v1/user_login_request_spec.rb
@@ -3,6 +3,12 @@ require 'rails_helper'
 describe 'Sessions API' do
   it 'sends back the api key for the user account' do
 
+    user = User.create!(email: "whatever@example.com",
+                        password: "password",
+                        password_confirmation: "password",
+                        api_key: SecureRandom.urlsafe_base64
+                        )
+
     post '/api/v1/sessions', params: {email: "whatever@example.com",
                                       password: "password",
                                       }
