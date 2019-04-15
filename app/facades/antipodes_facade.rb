c9f479eb7513144a3cc1eaaa37cb75ec320df34f
Julia Marco <marcootj@gmail.com>
2019-04-15 10:46:57 -0600

    Refactored antipodes facade, all tests are passing

--- app/facades/antipodes_facade.rb
+++ app/facades/antipodes_facade.rb
@@ -1,17 +1,26 @@
 class AntipodesFacade < LocationFacade
 
   def get_antipodes
-    # binding.pry
-    result_1 = service_1.find_antipodes(get_location)[:data]
-    lat_long = result_1[:attributes]
+    data = create_data(result_1, result_2)
+    service_2.create_antipode(data, @address)
+  end
+
+  def create_data(result_1, result_2)
     data = {}
     data[:id] = result_1[:id]
-    result = service_2.get_forecast(lat_long[:lat], lat_long[:long])
-    data[:summary] = result[:currently][:summary]
-    data[:temp] = result[:currently][:temperature]
-    data[:location] = result[:timezone]
-    # binding.pry
-    service_2.create_antipode(data, @address)
+    data[:summary] = result_2[:currently][:summary]
+    data[:temp] = result_2[:currently][:temperature]
+    data[:location] = result_2[:timezone]
+    return data
+  end
+
+  def result_1
+    service_1.find_antipodes(get_location)[:data]
+  end
+
+  def result_2
+    lat_long = result_1[:attributes]
+    service_2.get_forecast(lat_long[:lat], lat_long[:long])
   end
 
   def service_1
