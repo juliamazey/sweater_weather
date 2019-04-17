fba7527d67fb3c423650ed03fe460c8cb63624aa
Julia Marco <marcootj@gmail.com>
2019-04-16 18:37:31 -0600

    Final commit

--- spec/requests/api/v1/user_creation_request_spec.rb
+++ spec/requests/api/v1/user_creation_request_spec.rb
@@ -10,7 +10,18 @@ describe 'User API' do
 
     expect(response).to be_successful
     expect(response.status).to eq(201)
-    
+
     key = JSON.parse(response.body)
+  end
+
+  it 'sends back a 400 status if invalid' do
+
+    post '/api/v1/users', params: {email: "whatever@example.com",
+                                   password: "password",
+                                   password_confirmation: "invalid"
+                                  }
+
+    expect(response).to_not be_successful
+    expect(response.status).to eq(400)
   end
 end
